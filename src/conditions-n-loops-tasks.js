4af948d39629bb46e5c40d3af1edf793a2c319cb
natagetup <n.kulikouskaya@gmail.com>
2024-01-11 21:35:25 +0100

    feat: add shuffleChar basic implementation

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -377,8 +377,25 @@ function sortByAsc(arr) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let newStr = str;
+
+  for (let i = 0; i < iterations; i += 1) {
+    let rightStr = '';
+    let leftStr = '';
+
+    for (let j = 0; j < str.length; j += 1) {
+      if (j % 2 === 0) {
+        rightStr += newStr[j];
+      } else {
+        leftStr += newStr[j];
+      }
+    }
+
+    newStr = rightStr + leftStr;
+  }
+
+  return newStr;
 }
 
 /**
